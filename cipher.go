1b4a15ddf719e22bb4baf7d7d7adc2e069fa8e84
carlos.dasilva <carlos.dasilva@dafiti.com.br>
2019-02-20 21:11:56 -0300

    some code

--- cipher.go
+++ cipher.go
@@ -12,10 +12,20 @@ func cipher(phrase string, key int) {
 	}
 }
 
-func decipher(phase string, key int) {
-
+func decipher(phrase string, key int) {
+	for i := 0; i < len(phrase); i++ {
+		var c = []rune(phrase)[i]
+		for j := 0; j < key; j++ {
+			c = c - 1
+		}
+		fmt.Printf("%c", c)
+	}
 }
 
 func main() {
+	fmt.Println("Caesar Cipher: ")
+	fmt.Println("Cipher: ")
 	cipher("Olha que legal", 3);
+	fmt.Println("Decipher: ")
+	decipher("Rokd#txh#ohjdo",3);
 }
