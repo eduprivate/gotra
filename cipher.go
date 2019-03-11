ac2c362e2d7cf7d0d58278b4c63723b48a02bd33
carlos.dasilva <carlos.dasilva@dafiti.com.br>
2019-03-10 22:12:10 -0300

    some go code

--- cipher.go
+++ cipher.go
@@ -4,38 +4,42 @@ import (
 	"bufio"
 	"fmt" 
 	"errors"
+	"io"
+	"strings"
 	"os"		
 )
 
-func cipher(phrase string, key int) {
+func cipher(phrase string, key int32) string {
+	content := ""
 	for i := 0; i < len(phrase); i++ {
 		var c = []rune(phrase)[i]
-		for j := 0; j < key; j++ {
-			c = c + 1
-		}
+		c = c + key
 		fmt.Printf("%c", c)
+		content = content + string(c)
 	}
+	return content
 }
 
-func decipher(phrase string, key int) {
+func decipher(phrase string, key int32) string {
+	content := ""
 	for i := 0; i < len(phrase); i++ {
 		var c = []rune(phrase)[i]
-		for j := 0; j < key; j++ {
-			c = c - 1
-		}
+		c = c + key
 		fmt.Printf("%c", c)
+		content = content + string(c)
 	}
+	return content
 }
 
-func loadFile(fname string) ([]string, error) {
+func loadFile(fname string) (string, error) {
 	if fname == "" {
-		return nil, errors.New("Decripted file name cannot be emtpy.")
+		return "", errors.New("Decripted file name cannot be emtpy.")
 	}
 
 	// attempt to load dict file.
 	file, err := os.Open(fname)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	defer file.Close()
 
@@ -45,17 +49,62 @@ func loadFile(fname string) ([]string, error) {
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
 	}
-	return lines, scanner.Err()
+
+	content := ""
+	for _, value := range lines {
+		content = content + value
+	} 
+	fmt.Println(content)
+	return content, scanner.Err()
 }
 
-func writeFile(fname, content string) {
+func writeFile(fname, content string) error {
+	fo, err := os.Create(fname)
+	if err != nil {
+		return err
+	}
+	defer fo.Close()
 
+	_, err = io.Copy(fo, strings.NewReader(content))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func decriptFile(file_origen, file_destine string, key int32) error {
+	content, err := loadFile(file_origen)
+	
+	if err != nil {
+		return err
+	}
+
+	file_content := decipher(content, key)
+	writeFile(file_destine, file_content)
+	return nil
+}
+
+func criptFile(file_origen, file_destine string, key int32) error {
+	content, err := loadFile(file_origen)
+
+	if err != nil {
+		return err
+	}
+
+	file_content := cipher(content, key)
+	writeFile(file_destine, file_content)
+	return nil
 }
 
 func main() {
-	fmt.Println("Caesar Cipher: ")
-	fmt.Println("Cipher: ")
-	cipher("Olha que legal", 3);
-	fmt.Println("Decipher: ")
-	decipher("Rokd#txh#ohjdo",3);
+	// fmt.Println("Caesar Cipher: ")
+	// fmt.Println("Cipher: ")
+	// cipher("Olha que legal", 3);
+	// fmt.Println("Decipher: ")
+	// decipher("Rokd#txh#ohjdo",3);
+
+	criptFile("./textin.txt", "./textout.txt", 3)
+
+
 }
