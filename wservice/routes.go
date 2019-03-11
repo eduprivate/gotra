d9ac7e3bbf1555052583cfd71df1eccf2b412bdf
carlos.dasilva <carlos.dasilva@dafiti.com.br>
2019-03-10 22:32:42 -0300

    more some go code

--- wservice/routes.go
+++ wservice/routes.go
@@ -1,10 +1,6 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/gorilla/mux"
-)
+import "net/http"
 
 type Route struct {
 	Name        string
@@ -13,22 +9,7 @@ type Route struct {
 	HandlerFunc http.HandlerFunc
 }
 
-
 type Routes []Route
-
-func NewRouter() *mux.Router {
-
-	router := mux.NewRouter().StrictSlash(true)
-	for _, route := range routes {
-		router.
-			Methods(route.Method).
-			Path(route.Pattern).
-			Name(route.Name).
-			Handler(route.HandlerFunc)
-	}
-
-	return router
-}
 
 var routes = Routes{
 	Route{
@@ -42,6 +23,12 @@ var routes = Routes{
 		"GET",
 		"/todos",
 		TodoIndex,
+	},
+	Route{
+		"TodoCreate",
+		"POST",
+		"/todos",
+		TodoCreate,
 	},
 	Route{
 		"TodoShow",
